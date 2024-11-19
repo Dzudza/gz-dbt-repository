47cf51cf02e53effd57e2348999c4b2b6e716dfd
Zuzana Pietersen <zuzana.data.bootcamp@gmail.com>
2024-11-19 10:10:39 +0000

    removing duplicate payement from the query

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -10,11 +10,6 @@ orders as (
 
 ),
 
-payments as (
-
-    select * from {{ ref("stg_stripe__payments")}}
-),
-
 customer_orders as (
 
     select
@@ -22,10 +17,8 @@ customer_orders as (
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
         , count(order_id) as number_of_orders
-        , sum(payments.amount) as lifetime_value
+        , sum(amount) as lifetime_value   
     from orders
-
-    left join payments using (order_id)
     group by 1
 ),
 
@@ -37,12 +30,12 @@ final as (
         , customers.last_name
         , customer_orders.first_order_date
         , customer_orders.most_recent_order_date
-        , coalesce (customer_orders.number_of_orders, 0) as number_of_orders
+        , coalesce(customer_orders.number_of_orders, 0) as number_of_orders
         , customer_orders.lifetime_value
     from customers
-
+    
     left join customer_orders using (customer_id)
-
+    
 )
 
 select * from final
